6fe203d6e67ed268206c3eb45c1aa7ed975fa91f
ermitarojo <tincarandang@hotmail.com>
2019-11-18 21:10:24 +1300

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,13 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var myArray = [
+  "Apples",
+  "Bananas",
+  "Pears"
+];
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+document.body.innerHTML = randomItem;
